82b82ae6cb7543e2b7b05d82e84ce7b12a5eddc4
Marcos Molinelli <marcosmolinelli@gmail.com>
2025-06-17 21:45:03 -0300

    feat: :sparkles: PI-40 | Implementar roles de usuario
    https://proyectoespanta.atlassian.net/browse/PI-40?atlOrigin=eyJpIjoiNTljNmNkMDc4YTFlNGZiY2FiZDFlNTRjMjhkNDI5YTAiLCJwIjoiaiJ9

--- functions/login/usuarioLogin.js
+++ functions/login/usuarioLogin.js
@@ -2,14 +2,16 @@ require('dotenv').config();
 const dbService = require('../../services/dbService');
 const s3Service = require('../../services/s3Service');
 const UTILS = require('../../utils/utils');
+const jwt = require('jsonwebtoken');
+
+const SECRET = process.env.JWT_SECRET;
 
 module.exports.handler = async (event) => {
   try {
     console.log({ event });
 
     const body = JSON.parse(event.body);
-    const { dni, contrasenia } = body;
-
+    const { dni, contrasenia } = JSON.parse(event.body);
     // const { valid, message } = UTILS.validarLoginInput(usuario, contrasenia);
     // if (!valid) {
     //   return {
@@ -40,18 +42,32 @@ module.exports.handler = async (event) => {
       };
     }
 
-    // TODO: Generar JWT si querés
+    //JWT con rol incluido
+    const token = jwt.sign(
+      {
+        id: user.id,
+        nombre: user.nombre,
+        apellido: user.apellido,
+        dni: user.dni,
+        rol: user.rol, // ej: "alumno", "profesor", "admin"
+      },
+      SECRET,
+      { expiresIn: '2h' }
+    );
 
     return {
       statusCode: 200,
       body: JSON.stringify({
         message: 'Login exitoso',
+        token,
         user: {
           id: user.id || null, // S3 no tiene ID
           nombre: user.nombre,
           apellido: user.apellido,
           usuario: user.usuario || user.usuario, // en S3 usamos "usuario"
-        },
+          //agregar rol en s3
+          rol: user.rol || null, // agregamos el rol si viene de base
+        }, 
       }),
     };
   } catch (error) {
